4a56db72c017295b564df0495629f5613a67b240
Keyon Ashby <ashbykey000@warren.k12.in.us>
2024-08-23 09:21:28 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+miles = " I ran 3."
+minutes = " It took 60 mins"
+mph = 3.0
+print("How many miles did you run?",miles, sep="")
+print("How many minutes did it take?",minutes, sep="")
+print("Speed in mph:",mph, sep="")
