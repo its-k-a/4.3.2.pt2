bbd628c5f7d9cbe43d8a4004508abc73fa2b0454
Keyon Ashby <ashbykey000@warren.k12.in.us>
2024-08-23 10:01:18 -0400

    second commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 miles = " I ran 3."
-minutes = " It took 60 mins"
+minutes = " It took 60 mins per mile."
+hours= 180/60 #180 mins divided by 60= the amount of hours which is 3
 mph = 3.0
 print("How many miles did you run?",miles, sep="")
 print("How many minutes did it take?",minutes, sep="")
